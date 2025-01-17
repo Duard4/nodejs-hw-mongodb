43d18f34b6c4bce05f2f640b06872fb8686a2428
Eduard <parhomenkoeduard75@gmail.com>
2025-01-17 14:48:32 +0200

    feat: implement CRUD operations for contacts with error handling and routing improvements

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import studentsRouter from './routers/contacts.js';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getContactById, getContacts } from './services/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
 
-  app.use(express.json());
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
+    }),
+  );
   app.use(cors());
 
   app.use(
@@ -26,44 +33,11 @@ export const setupServer = () => {
     });
   });
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getContacts();
+  app.use(studentsRouter);
 
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact)
-      res.status(404).json({
-        message: `Contact not found ${contactId}`,
-      });
-
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
